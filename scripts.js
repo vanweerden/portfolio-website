1b3a246bebcbd095d5ea206bbe24e2f4c8087f3f
vanweerden <ando.vanweerden@gmail.com>
2020-02-09 17:00:03 +0800

    Changed dropdown button text to '>'. It also now changes when clicked.

--- scripts.js
+++ scripts.js
@@ -3,7 +3,11 @@ function toggleDropMenu() {
   const dropContent = document.getElementById("dropdown-content");
   // "block" or "none"
   const dropState = dropContent.style.display;
+  const dropButton = document.getElementById("dropbtn");
 
   // toggles display
   dropContent.style.display = dropState == "block" ? "none" : "block";
+
+  // Changes text inside button
+  dropButton.textContent = dropButton.textContent == ">" ? "v" : ">";
 }
