6663e7738490b7ccebc522a4dbfbc5f7b83efb25
vanweerden <ando.vanweerden@gmail.com>
2020-06-20 08:49:44 +0800

    Finished coding client-side form  validation

--- scripts.js
+++ scripts.js
@@ -14,6 +14,15 @@ function toggleDropMenu() {
 }
 
 /* FORM VALIDATION */
+// when user clicks Send button:
+// if any field is not valid
+// 1 error shows up below that field
+  // select each error element
+  // change text content of that element
+  // change css of that element to display
+// 2 submission is prevented by preventDefault
+// if all fields are valid, form submits (test: console.log)
+
 const form = document.getElementById('contact-form');
 const name = document.getElementById('form-name');
 const email = document.getElementById('form-email');
@@ -24,15 +33,49 @@ const emailError = document.getElementById('email-error');
 const msgError = document.getElementById('message-error');
 
 form.addEventListener('submit', function (event) {
+  console.log('Missing?', msg.validity.valueMissing);
+  console.log('Valid?', msg.validity.valid);
   // Form submits if all fields are valid
 
-  // If not, display appropriate error message
-  if (!name.validity.valid) {
-    showError();
+  // Clear any previous error messages if fixed by user
+  if (name.validity.valid) {
+    nameError.innerHTML = '';
+    nameError.style.display = 'none';
+  }
+  if (email.validity.valid) {
+    emailError.innerHTML = '';
+    emailError.style.display = 'none';
+  }
+  if (msg.validity.valid) {
+    msgError.innerHTML = '';
+    msgError.style.display = 'none';
+  }
+
+  // If a field is invalid, display appropriate error message and prevent submission
+  if (!name.validity.valid
+      || !email.validity.valid
+      || !msg.validity.valid) {
+    handleErrors();
     event.preventDefault();
   }
 });
 
-function showError(field) {
+function handleErrors() {
+  if (name.validity.valueMissing) {
+    nameError.textContent = 'Please include your name.';
+    nameError.style.display = 'inline-block';
+  }
 
+  if (email.validity.valueMissing) {
+    emailError.textContent = 'Please enter an e-mail address.';
+    emailError.style.display = 'inline-block';
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = 'Entered value needs to be an e-mail address.';
+    emailError.style.display = 'inline-block';
+  }
+
+  if (msg.validity.valueMissing) {
+    msgError.textContent = 'Please write a message.';
+    msgError.style.display = 'inline-block';
+  }
 }
