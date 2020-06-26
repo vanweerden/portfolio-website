6d94c8dbe5e302d1d42ed056e395a333d0099109
vanweerden <ando.vanweerden@gmail.com>
2020-06-26 11:58:57 +0800

    Added PHP script to send form data to my e-mail address

---
+++ form-to-email.php
@@ -0,0 +1,31 @@
+<?php
+$name = $email = $subject = $message = "";
+
+// Assign input to variables
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = cleanup_input($_POST["name"]);
+  $visitor_email = cleanup_input($_POST["email"]);
+  $subject = cleanup_input($_POST["subject"]);
+  $message = cleanup_input($_POST["message"]);
+}
+
+// Clean up data
+function cleanup_input($data) {
+  $data = trim($data);
+  $data = stripslash($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+// Send as e-mail
+$email_from = "andrew@vanweerden.me";
+$email_subject = $subject;
+$email_body = "You have received a new message from $name\n".
+                  "Here is the message:\n $message";
+
+$to = "ando.vanweerden@gmail.com";
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email\r\n";
+
+mail($to,$subject,$email_body,$headers);
+?>
