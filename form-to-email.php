f240211c4831cb0278a9763217b368b2a684ee9c
vanweerden <ando.vanweerden@gmail.com>
2020-06-26 12:08:08 +0800

    Testing php

--- form-to-email.php
+++ form-to-email.php
@@ -17,15 +17,20 @@ function cleanup_input($data) {
   return $data;
 }
 
+echo $name;
+echo $visitor_email;
+echo $subject;
+echo $message;
+
 // Send as e-mail
-$email_from = "andrew@vanweerden.me";
-$email_subject = $subject;
-$email_body = "You have received a new message from $name\n".
-                  "Here is the message:\n $message";
-
-$to = "ando.vanweerden@gmail.com";
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email\r\n";
-
-mail($to,$subject,$email_body,$headers);
+// $email_from = "andrew@vanweerden.me";
+// $email_subject = $subject;
+// $email_body = "You have received a new message from $name\n".
+//                   "Here is the message:\n $message";
+//
+// $to = "ando.vanweerden@gmail.com";
+// $headers = "From: $email_from \r\n";
+// $headers .= "Reply-To: $visitor_email\r\n";
+//
+// mail($to,$subject,$email_body,$headers);
 ?>
