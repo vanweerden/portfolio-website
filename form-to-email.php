023fbb8363281a745a8c496b1db28863c7127836
vanweerden <ando.vanweerden@gmail.com>
2020-06-26 12:44:04 +0800

    Testing mail() function

--- form-to-email.php
+++ form-to-email.php
@@ -1,24 +1,24 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-$name = $email = $subject = $message = "";
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+// $name = $email = $subject = $message = "";
 
 // Assign input to variables
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = cleanup_input($_POST["name"]);
-  $visitor_email = cleanup_input($_POST["email"]);
-  $subject = cleanup_input($_POST["subject"]);
-  $message = cleanup_input($_POST["message"]);
-}
+// if($_SERVER["REQUEST_METHOD"] == "POST") {
+//   $name = cleanup_input($_POST["name"]);
+//   $visitor_email = cleanup_input($_POST["email"]);
+//   $subject = cleanup_input($_POST["subject"]);
+//   $message = cleanup_input($_POST["message"]);
+// }
 
 // Clean up data
-function cleanup_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+// function cleanup_input($data) {
+//   $data = trim($data);
+//   $data = stripslashes($data);
+//   $data = htmlspecialchars($data);
+//   return $data;
+// }
 
 // Send as e-mail
 // $email_from = "andrew@vanweerden.me";
