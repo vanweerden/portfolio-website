b1b13a3b9acafca1a7bc5153e1b25b3ba887657e
vanweerden <ando.vanweerden@gmail.com>
2020-06-26 14:12:19 +0800

    Added success message on form submission

--- form-to-email.php
+++ form-to-email.php
@@ -1,35 +1,49 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Message sent successfully!</title>
+  </head>
+  <body>
 
-// Assign input to variables
-$name = $visitor_email = $subject = $message = "";
+  <?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = cleanup_input($_POST["name"]);
-  $visitor_email = cleanup_input($_POST["email"]);
-  $subject = cleanup_input($_POST["subject"]);
-  $message = cleanup_input($_POST["message"]);
-}
+  // Assign input to variables
+  $name = $visitor_email = $subject = $message = "";
 
-// Clean up data
-function cleanup_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = cleanup_input($_POST["name"]);
+    $visitor_email = cleanup_input($_POST["email"]);
+    $subject = cleanup_input($_POST["subject"]);
+    $message = cleanup_input($_POST["message"]);
+  }
 
-// Send as e-mail
-$to = "ando.vanweerden@gmail.com";
+  // Clean up data
+  function cleanup_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
 
-$email_from = "andrew@vanweerden.me";
-$email_body = "You have received a new message from $name\n".
-                  "Here is the message:\n $message";
+  // Send as e-mail
+  $to = "ando.vanweerden@gmail.com";
 
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email\r\n";
+  $email_from = "andrew@vanweerden.me";
+  $email_body = "You have received a new message from $name\n".
+                    "Here is the message:\n $message";
 
-mail($to,$subject,$email_body,$headers);
-?>
+  $headers = "From: $email_from \r\n";
+  $headers .= "Reply-To: $visitor_email\r\n";
+
+  $success = mail($to,$subject,$email_body,$headers);
+
+  // If no errors, display success message
+  $message = $success ? 'Thank you for your e-mail! I will respond as soon as possible.' : 'There was a problem sending your message. Terribly sorry about that!';
+  echo "<div class='message'>$message</div>";
+  ?>
+
+  </body>
+</html>
