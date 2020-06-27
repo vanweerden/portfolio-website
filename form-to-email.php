73cbddb134d10ebd3d97cf41366d48f2c6190747
vanweerden <ando.vanweerden@gmail.com>
2020-06-27 08:45:08 +0800

    Added CSS styling to php page

--- form-to-email.php
+++ form-to-email.php
@@ -5,45 +5,52 @@
     <title></title>
   </head>
   <body>
-    <?php
-    // Assign input to variables
-    $name = $visitor_email = $subject = $message = "";
+    <div class='page'>
+      <div class='message-container'>
+        <?php
+        // Assign input to variables
+        $name = $visitor_email = $subject = $message = "";
 
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-      $name = cleanup_input($_POST["name"]);
-      $visitor_email = cleanup_input($_POST["email"]);
-      $subject = cleanup_input($_POST["subject"]);
-      $message = cleanup_input($_POST["message"]);
-    }
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+          $name = cleanup_input($_POST["name"]);
+          $visitor_email = cleanup_input($_POST["email"]);
+          $subject = cleanup_input($_POST["subject"]);
+          $message = cleanup_input($_POST["message"]);
+        }
 
-    // Clean up data
-    function cleanup_input($data) {
-      $data = trim($data);
-      $data = stripslashes($data);
-      $data = htmlspecialchars($data);
-      return $data;
-    }
+        // Clean up data
+        function cleanup_input($data) {
+          $data = trim($data);
+          $data = stripslashes($data);
+          $data = htmlspecialchars($data);
+          return $data;
+        }
 
-    // Send as e-mail
-    $to = "ando.vanweerden@gmail.com";
+        // Send as e-mail
+        $to = "ando.vanweerden@gmail.com";
 
-    $email_from = "andrew@vanweerden.me";
-    $email_body = "You have received a new message from $name\n".
-                      "Here is the message:\n $message";
+        $email_from = "andrew@vanweerden.me";
+        $email_body = "You have received a new message from $name\n".
+                          "Here is the message:\n $message";
 
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $visitor_email\r\n";
+        $headers = "From: $email_from \r\n";
+        $headers .= "Reply-To: $visitor_email\r\n";
 
-    $success = mail($to,$subject,$email_body,$headers);
+        $success = mail($to,$subject,$email_body,$headers);
 
-    // Option 2: If no errors, display success message (then redirect?)
-    if ($success == true) {
-      $response = "Thank you for your message! I will respond as soon as possible.";
-    } else {
-      $response = "Something went wrong and your message was not sent successfully.";
-    }
-    
-    echo "<div class='message'>", $response, "</div>";
-    ?>
+        // Option 2: If no errors, display success message (then redirect?)
+        if ($success == true) {
+          $response = "Thank you for your message! I will respond as soon as possible.";
+        } else {
+          $response = "Something went wrong and your message was not sent successfully.";
+        }
+
+        echo "<div class='message'>", $response, "</div>";
+        ?>
+        <div class='link-container'>
+          <a href='https://vanweerden.me'>Click here to back to the main page</a>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
